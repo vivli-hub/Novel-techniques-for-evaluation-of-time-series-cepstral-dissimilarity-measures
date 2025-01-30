9330e0ec6f9e3fb14d286232d3caf1906fbe23ba
vivli-hub <67989763+vivli-hub@users.noreply.github.com>
2025-01-31 11:24:19 +1300

    Update windowed_per.m

--- code/matlab/windowed_per.m
+++ code/matlab/windowed_per.m
@@ -1,17 +1,17 @@
 function windowed_per(fname, vecw, dis)
 %input
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% fname           % folder name
-% vecw             % Weight martix
-% dis              % sq-squared Eulcidean distance
-%                  % eu-squared Eulcidean distance
+% fname            % folder name
+% vecw             % Matrix
+% dis              % 'sq' - squared distance
+%                  % 'eu' - distance
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Load the simulated data
 fname1 = strcat(fname,'simdata.mat');
 load(fname1,'Ymata', 'Ymatb', 'DistTrue');
 
-%Calculate the estimated cepstral ceofficients
+%Calculate the estimated cepstral coefficients
 CEPaWP = comp_CEP_WP(Ymata);
 CEPbWP = comp_CEP_WP(Ymatb);
 
@@ -33,15 +33,14 @@ ResWP{1,1} = 'Rectangle window';
 ResWP{2,1} = 'Hann';
 
 
-%Find out the bias of the estimated distance (mean and std.)
+%Mean and var estimation errors
 MD = MatDistWP-DistTrue;
 for ind=1:2
     ResWP{ind,2} = mean(MD(ind,:));
     ResWP{ind,3} = var(MD(ind,:),1);
 end
 
-% Save all useful results in the 'resultsWP.mat', including the estimated
-% cepstral coefficients, the estiamted distance and bias of that.
+% Save all useful results in the 'resultsWP.mat'
 fname2 = strcat(fname,'resultsWP.mat');
 save(fname2, 'CEPaWP', 'CEPbWP', 'MatDistWP',"ResWP");
 
@@ -49,14 +48,14 @@ save(fname2, 'CEPaWP', 'CEPbWP', 'MatDistWP',"ResWP");
 end %function
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Calculate the estimated copstral coefficients with the window peridogram
+% Calculate the estimated copstral coefficients with the windowed peridogram
 function CEP = comp_CEP_WP(Y)
 
 [N, Nr] = size(Y);
 CEP     = Inf(2,N,Nr);
 
 for run=1:Nr
-    [CEP(1,:,run), ~] = wp( Y(:,run), 'rect' ); % without window periodogram
+    [CEP(1,:,run), ~] = wp( Y(:,run), 'rect' ); % rectangular window
     [CEP(2,:,run), ~] = wp( Y(:,run), 'hann' ); % Hann window
 end
 
@@ -65,17 +64,12 @@ end %function
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [c_p, Phi_WP_mod] = wp(y, v)
 
-
-
  N = length(y);
 
  if strcmp(v, 'rect')
-     
-      %Phi_WP = periodogramse(y,ones(1, N),N);
-       [Phi_WP, freq] = periodogram(y, ones(1, N), (0:2*pi/N:2*pi*(N-1)/N));
+       [Phi_WP, ~] = periodogram(y, ones(1, N), (0:2*pi/N:2*pi*(N-1)/N));
  elseif strcmp(v, 'hann')
-     %Phi_WP = periodogramse(y,hann(N),N);
-       [Phi_WP, freq] = periodogram(y, hann(N), (0:2*pi/N:2*pi*(N-1)/N));
+       [Phi_WP, ~] = periodogram(y, hann(N), (0:2*pi/N:2*pi*(N-1)/N));
  else
         fprintf('error \n ')
  end
@@ -84,7 +78,7 @@ function [c_p, Phi_WP_mod] = wp(y, v)
  Phi_WP_mod = flipud([Phi_WP(L/2+1:L); Phi_WP(1:L/2)]);
  c_p = comp_cep_wp(Phi_WP);
 
-end %funtion
+end %function
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -94,8 +88,7 @@ function c_p = comp_cep_wp(Phi_p)
 %output:
 %   c_p    = cepstral coeff
 
-% Check the estimated cepstral coefficients and only keep the real part of
-% that.
+% Check the estimated cepstral coefficients
 c_p = ifft(log(Phi_p));
  if max(abs(imag(c_p)))<10^(-4)
     c_p = real(c_p);
@@ -106,5 +99,7 @@ c_p = ifft(log(Phi_p));
  end
 
 end %function
+
+
 
 
